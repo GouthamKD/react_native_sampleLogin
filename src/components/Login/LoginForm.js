b27fa48ebbe3c58b91807712c32099f8c3cf05b3
Goutham K D <Goutham.KD@Lnttechservices.com>
2017-12-20 19:39:15 +0530

    WIP

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -2,20 +2,47 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Alert, Button, StyleSheet, StatusBar } from 'react-native';
 
-const onButtonPress = () => {
-Alert.alert('Login button is clicked!!');
-}
+// const onButtonPress = () => {
+//     Alert.alert('Login button is clicked!!');
+//     Alert.alert('email is',state.email);
+//     Alert.alert('passsword is',this.state.password);
+// }
 
 export default class LoginForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    onLoginPress=()=>{
+   const {email,password} =this.state;
+//    Alert.alert('email is',email);
+   if(this.state.email==='test@gmail.com' && this.state.password==='test')
+   {
+       Alert.alert('succesfully logged in!!!');
+       return;
+   }
+   Alert.alert('please try with valid credentials!!');
+   this.emailInput.clear();
+   this.passwordInput.clear();
+   return;
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <StatusBar barStyle="light-content" />
                 <TextInput style={styles.input}
+                    onChangeText={email=>this.setState({email})}
+                    value={this.state.text}
                     autoCapitalize="none"
                     onSubmitEditing={() => this.passwordInput.focus()}
                     autoCorrect={false}
+                    ref={input => { this.emailInput = input }}
                     enablesReturnKeyAutomatically={true}
                     keyboardType='email-address'
                     returnKeyType="next"
@@ -23,12 +50,14 @@ export default class LoginForm extends Component {
                     placeholderTextColor='rgba(225,225,225,0.7)' />
 
                 <TextInput style={styles.input}
-                    returnKeyType="go" ref={(input) => this.passwordInput = input}
+                    onChangeText={password=>this.setState({password})}
+                    returnKeyType="go" 
+                    ref={input => { this.passwordInput = input }}
                     placeholder='Password'
                     placeholderTextColor='rgba(225,225,225,0.7)'
                     secureTextEntry />
                 {/*   <Button onPress={onButtonPress} title = 'Login' style={styles.loginButton} /> */}
-                <TouchableOpacity style={styles.buttonContainer} onPress={onButtonPress}>
+                <TouchableOpacity style={styles.buttonContainer} onPress={this.onLoginPress}>
                     <Text style={styles.buttonText}>LOGIN</Text>
                 </TouchableOpacity>
             </View>
